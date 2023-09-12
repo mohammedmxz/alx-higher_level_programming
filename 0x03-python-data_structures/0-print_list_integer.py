464ad1919646f3aabb7ac8bac997b710efd7394c
mohammedmxz <mohammedmxzk@gmail.com>
2023-09-12 01:01:51 +0000

    just added

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,5 +4,6 @@ def print_list_integer(my_list=[]):
     for item in my_list:
         print("{:d}".format(item))
 
+
 if __name__ == "__main__":
     print_list_integer()
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,6 @@ def element_at(my_list, idx):
     if idx >= 0 and idx < len(my_list):
         return my_list[idx]
 
+
 if __name__ == "__main__":
     element_at()
