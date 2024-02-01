7f220a913e00413a05abccc62827875da4a30d51
mohammedmxz <mohammedmxzk@gmail.com>
2024-02-01 00:51:51 +0000

    just added

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
